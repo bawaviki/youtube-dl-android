2f3a4a5cb698ad2dae2859772119ddbd9e74e7c1
bawaviki <vikasbawa5@gmail.com>
2020-11-06 13:27:51 +0530

    Now download callback provide both size and rate

--- library/src/main/java/com/bawaviki/youtubedl_android/DownloadProgressCallback.java
+++ library/src/main/java/com/bawaviki/youtubedl_android/DownloadProgressCallback.java
@@ -1,5 +1,5 @@
 package com.bawaviki.youtubedl_android;
 
 public interface DownloadProgressCallback {
-    void onProgressUpdate(float progress, long etaInSeconds);
+    void onProgressUpdate(float progress, long size, long rate, long etaInSeconds);
 }
